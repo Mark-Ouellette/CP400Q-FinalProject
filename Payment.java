11c4d421dfd548295d7f0f0db83930d20a37b7d5
Mark11997 <Ouel4390@mylaurier.ca>
2017-03-29 13:34:54 -0400

    test

---
+++ Payment.java
@@ -0,0 +1,14 @@
+package ca.wlu.markouellette.cp400q_finalproject;
+
+/**
+ * Created by Mark on 2017-03-29.
+ */
+
+public class Payment {
+
+    String mLabel;
+
+    public Payment(String l){
+        mLabel = l;
+    }
+}
