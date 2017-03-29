69224d08857dc5f8bee7c4ae11ca900b60c5a9e7
Mark11997 <Ouel4390@mylaurier.ca>
2017-03-29 13:21:35 -0400

    Initial commit by m-nizzle

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package ca.wlu.markouellette.cp400q_finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("hi");
+    }
+}
